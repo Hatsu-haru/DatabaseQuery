6eddb4a1617d6ab82988d109c8da30c0107dff91
admin <huiaini015@gmail.com>
2020-10-19 20:55:40 +0800

    TEMP

--- app/src/main/java/com/example/databasequery/DataBaseHelper.kt
+++ app/src/main/java/com/example/databasequery/DataBaseHelper.kt
@@ -1,30 +1,40 @@
 package com.example.databasequery
 
+import android.R.id
+import android.app.DownloadManager
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.net.Uri
 import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 
-class DataBaseHelper (val context: Context, name: String, version: Int) : SQLiteOpenHelper(context, name,null, version){
 
-    private val createToolsBase = "create table Tools (" +
+class DataBaseHelper(val context: Context, name: String, version: Int) : SQLiteOpenHelper(
+    context, name, null, version) {
+    private val createSearch = "create table Search (" +
             " id integer primary key autoincrement," +
             "name text," +
+            "analysis_type text)"
+
+    private val createInformation = "create table Information (" +
+            " name text primary key," +
             "author text," +
-            "analysis_type text," +
             "download_address text," +
             "introduction text)"
 
     override fun onCreate(db: SQLiteDatabase?) {
         if (db != null) {
-            db.execSQL(createToolsBase)
+            db.execSQL(createSearch)
+            db.execSQL(createInformation)
         }
         Toast.makeText(context, "Create succeeded", Toast.LENGTH_SHORT).show()
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        // TODO Auto-generate database
+        db.execSQL("drop table if exists Search")
+        db.execSQL("drop table if exists Information")
+        onCreate(db)
         Toast.makeText(context, "Update succeeded", Toast.LENGTH_SHORT).show()
     }
-
 }
--- app/src/main/java/com/example/databasequery/MainActivity.kt
+++ app/src/main/java/com/example/databasequery/MainActivity.kt
@@ -1,25 +1,43 @@
 package com.example.databasequery
 
+import android.app.DownloadManager
+import android.content.ContentValues
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val dbHelper = DataBaseHelper(this, "ToolsList.db", 1)
-        dbHelper.writableDatabase
         button1.setOnClickListener{
             val intent = Intent(this,ToolsQuery::class.java)
             startActivity(intent)
         }
         button2.setOnClickListener{
-            val stringUrl = ""
-            val version = DownloadDataBase(stringUrl)
-            val dbHelper = DataBaseHelper(this, "ToolsList.db", version)
-            dbHelper.writableDatabase
+            val dbHelper = DataBaseHelper(this, "ToolsList.db", 1)
+            val db = dbHelper.writableDatabase
+            val values1 = ContentValues().apply {
+                put("name", "BWA")
+                put("analysis_type", "RNA")
+            }
+            db.insert("Search", null, values1)
+            val values2 = ContentValues().apply {
+                put("name", "BWA")
+                put("analysis_type", "DNA")
+            }
+            db.insert("Search", null, values2)
+            val values3 = ContentValues().apply {
+                put("name", "STAR")
+                put("analysis_type", "RNA")
+            }
+            db.insert("Search", null, values3)
+         //   val version = downloadDataBase()
+         //   val dbHelper = DataBaseHelper(this, "ToolsList.db", version)
+         //   dbHelper.writableDatabase
         }
         button3.setOnClickListener{
             val intent = Intent(this,ToolsListActivity::class.java)
@@ -31,8 +49,13 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun DownloadDataBase(stringUrl: String): Int {
-        //待编辑
-        return 0
+    private fun downloadDataBase(): Int {
+        val dManager: DownloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        val request = DownloadManager.Request(Uri.parse("https://raw.githubusercontent.com/Hatsu-haru/Temp/main/version2.db"))
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED )
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI)
+        request.setDestinationInExternalFilesDir(this, "/data/data/com.example.databasequery/databases/","test.db")
+        dManager.enqueue(request)
+        return 2
     }
 }
