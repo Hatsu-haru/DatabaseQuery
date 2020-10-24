2c15ba91caad67f4b36963d98effe3f8bca79acf
admin <huiaini015@gmail.com>
2020-10-24 19:07:32 +0800

    2020.10.24.1

--- app/src/main/java/com/example/databasequery/ToolsListActivity.kt
+++ app/src/main/java/com/example/databasequery/ToolsListActivity.kt
@@ -7,6 +7,10 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_tools_list.*
 import kotlinx.android.synthetic.main.activity_tools_query.*
 import kotlinx.android.synthetic.main.activity_tools_query.datalist
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.lang.Exception
+import kotlin.concurrent.thread
 
 class ToolsListActivity : AppCompatActivity() {
 
@@ -27,11 +31,21 @@ class ToolsListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tools_list)
-        initData()
+        getListData()
         val adapter = ToolAdapter(this, R.layout.tool_search_item, toolList)
         listview.adapter = adapter
     }
-    private fun initData(){
+    private fun getListData(){
+        thread {
+            try {
+                val client = OkHttpClient()
+                val request = Request.Builder().url("http://localhost/test.php").build()
+                val response = client.newCall(request).execute()
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+
         repeat(20) {
             toolList.add(Tool(2,"a","DNA"))
             toolList.add(Tool(1, "b", "RNA"))
